27b9247f7cfba59436f21cdd1e0ecbc36f5ae80f
Daniil Snadkin <dsnadkin@gmail.com>
2020-12-20 21:54:01 +0300

    Snadkin:Added some tables, TODO

--- main/models.py
+++ main/models.py
@@ -8,3 +8,28 @@ class User(AbstractUser):
 
 class UserSettings(models.Model):
     user = models.OneToOneField(to=User, on_delete=models.CASCADE)
+
+class Voting(models.Model):
+    author = models.IntegerField()
+    name = models.CharField()
+    description = models.CharField()
+    type = models.IntegerField()
+    published = models.DateTimeField()
+    finished = models.DateTimeField()
+    is_active = models.BooleanField()
+
+class VoteVariant(models.Model):
+    voting = models.IntegerField()
+    description = models.CharField()
+
+class VoteFact(models.Model):
+    author = models.IntegerField()
+    variant = models.IntegerField()
+    created = models.DateTimeField()
+
+class Complaint(models.Model):
+    author = models.IntegerField()
+    voting = models.IntegerField()
+    description = models.CharField()
+    status = models.IntegerField()
+
