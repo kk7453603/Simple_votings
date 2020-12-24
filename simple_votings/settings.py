837d73dfe9b9ec2770b0047c5a427678ef57d042
Mishkina Daria <dasha.mishkina117@gmail.com>
2020-12-24 21:16:55 +0300

    Mishkina: add secret key

--- simple_votings/settings.py
+++ simple_votings/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Далее полученное значение подставляется в соответствующую переменную
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'Insert secret key here and uncomment this variable'
+SECRET_KEY = '0(gqmt3((e1%+b%2pl^78yel-iu221kmmwc-69ey=$u#aj9ehp'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
