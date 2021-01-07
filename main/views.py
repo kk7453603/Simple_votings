771d019ce814df67316f449f42035755b7e733f3
Komarov Kirill <kk7453603@gmail.com>
2021-01-07 20:43:10 +0300

    added bd connection

--- main/views.py
+++ main/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 from main.models import Voting
 from .forms import *
 
+
 def get_menu_context():
     return [
         {'url_name': 'index', 'name': 'Главная'},
@@ -34,9 +35,9 @@ def time_page(request):
 
 
 def voting_page(request):
-    required_id = 2   # тут что нибудь умное придумаем
+    required_id = 2  # тут что нибудь умное придумаем
     context = {
-        'voting' : Voting.objects.filter(id=required_id),
+        'voting': Voting.objects.filter(id=required_id),
     }
     return render(request, 'pages/voting.html', context)
 
@@ -44,20 +45,22 @@ def voting_page(request):
 def voting_list_page(request):
     # author, name, description
     context = {
-        'history' : Voting.objects.all()
+        'history': Voting.objects.all()
     }
     return render(request, 'pages/voting_list.html', context)
 
+
 def voting_creation_page(request):
-    context={}
-    user=request.user
+    context = {}
+    user = request.user
     if request.method == 'POST':
-        form=Addform(request.POST)
+        form = Addform(request.POST)
         if form.is_valid():
-            #context['form']=form
-            pass
+            context['status'] = 1
+            adder = Voting(author=1, name=form.verh, author_name=user.USERNAME_FIELD, description=form.content, type=1,
+                           published=datetime.now(), finished=datetime.now(), is_active=1)
+            adder.save()
         else:
-            #form=Addform()
-            #context['form'] = form
-            pass
+            context['status'] = 0
+
     return render(request, 'pages/add_votings.html', context)
