046feadd347a0f016e977251adb061d6bdf55f4c
Daniil Snadkin <dsnadkin@gmail.com>
2021-01-26 21:20:41 +0300

    Snadkin: Admin panel

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from main.models import Voting, VoteVariant, Complaint
+
+admin.site.register(Voting)
+admin.site.register(VoteVariant)
+admin.site.register(Complaint)
